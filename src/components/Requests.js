2b5be68a73d8144d489bacb89b13c1aa2a6b38cb
Hellon Canella <helloncanella@gmail.com>
2016-09-14 10:39:57 -0300

    ok

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -17,14 +17,50 @@ export default class Requests extends Component {
 
             content = (
                 <div className="requests-list">
-                    <div className="request-item">                    
-                        <div className="col-6-12 left details">
-                            <i className="fa fa-caret-right"></i> Details | Anfrage von Hellon Canella | Zugewiesen an Nicht zugewiesen | Emmendingen | Medical | Dentist
-                        </div>                    
-                        <div className="col-6-12 right when">
-                            6 hours ago
-                        </div>                    
+                    <div className="request-item">
+                        <div className="details">                    
+                            <div className="col-6-12 left">
+                                <i className="fa fa-caret-right"></i> Details | Anfrage von Hellon Canella | Zugewiesen an Nicht zugewiesen | Emmendingen | Medical | Dentist
+                            </div>                    
+                            <div className="col-6-12 right when">
+                                6 hours ago
+                            </div>
+                        </div>
+                         <div className="description">                    
+                            <div className="col-12-12 left ">
+                                <p></p>
+                                <p>dfçajçlkfdçasdjf jflsakjçf</p>
+                            </div>                    
+                        </div>
+                        <div className="comments">
+                            <div className="col-12-12 left ">
+                               <p className="micro">1 comment</p>
+                            </div>                      
+                        </div>                     
                     </div>
+
+                    <div className="request-item">
+                        <div className="details">                    
+                            <div className="col-6-12 left">
+                                <i className="fa fa-caret-right"></i> Details | Anfrage von Hellon Canella | Zugewiesen an Nicht zugewiesen | Emmendingen | Medical | Dentist
+                            </div>                    
+                            <div className="col-6-12 right when">
+                                6 hours ago
+                            </div>
+                        </div>
+                         <div className="description">                    
+                            <div className="col-12-12 left ">
+                                <p></p>
+                                <p>dfçajçlkfdçasdjf jflsakjçf</p>
+                            </div>                    
+                        </div>
+                        <div className="comments">
+                            <div className="col-12-12 left ">
+                               <p className="micro">1 comment</p>
+                            </div>                      
+                        </div>                     
+                    </div>
+                    
                 </div>
             )
 
